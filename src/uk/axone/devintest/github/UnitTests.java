0e7b2b9742ba92b6f7a73a6eaf02a99a9dfca869
ramduvvuri <ram@axone-tech.uk>
2020-03-29 08:46:43 +0100

    committing my project firsttime

---
+++ src/uk/axone/devintest/github/UnitTests.java
@@ -0,0 +1,11 @@
+package uk.axone.devintest.github;
+
+import org.junit.Test;
+
+public class UnitTests {
+    
+    @Test
+    public void test1(){
+        System.out.println("Hello - running test 1");
+    }
+}
