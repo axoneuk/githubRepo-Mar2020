38e0a272e53d677978a9217db4a8920efa59287c
ramduvvuri <ram@axone-tech.uk>
2020-03-29 08:58:25 +0100

    Added Test2

--- src/uk/axone/devintest/github/UnitTests.java
+++ src/uk/axone/devintest/github/UnitTests.java
@@ -3,9 +3,14 @@ package uk.axone.devintest.github;
 import org.junit.Test;
 
 public class UnitTests {
-    
+
     @Test
     public void test1(){
         System.out.println("Hello - running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Hello - running test 2");
     }
 }
